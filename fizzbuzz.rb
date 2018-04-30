a0fc1c5df3e8e8a15ca94f6d491f525e50b44f67
Carlos Miguel Pimentel <eleanorisyuyu@gmail.com>
2018-04-30 09:33:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(num)
   if num % 15 == 0
-    puts "Fizzbuzz"
+    "Fizzbuzz"
   elsif
     num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif
     num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else
     nil
 end
