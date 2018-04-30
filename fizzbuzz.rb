11c78aea7e843fc36b15506d8f307527f2a624b8
Carlos Miguel Pimentel <eleanorisyuyu@gmail.com>
2018-04-30 09:30:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,13 @@ def fizzbuzz(num)
     "Fizzbuzz"
   end
 
-  elsif num % 3 == 0
+  elsif 
+    num % 3 == 0
     "Fizz"
   end
 
-  elsif num % 5 == 0
+  elsif 
+    num % 5 == 0
     "Buzz"
   end
 
